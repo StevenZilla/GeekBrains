a4640325e6cecc2c7cb3f406cbe8ae26d86f4646
Steven <stepanwinzaycev@gmail.com>
2024-09-28 14:53:24 +0300

    Add Python/Homeworks/Lesson_1/task_1.py

---
+++ Python/Homeworks/Lesson_1/task_1.py
@@ -0,0 +1,13 @@
+# Инициализация данных
+a = 8
+b = 10
+c = 12
+d = 18
+
+# Решение выражения
+numerator = ((-3 + a ** 2) * b - 2 ** 3)
+denumerator = (c - 2 * d)
+result = numerator / denumerator
+
+# Вывод результата
+print(result)
